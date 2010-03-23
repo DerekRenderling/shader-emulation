5abcf630b2e54181b425407d725d9d8bb9a3b78b
James Halliday <substack@gmail.com>
2010-03-22 16:13:13 -0800

    lawlor's changes

--- emulate.cpp
+++ emulate.cpp
@@ -147,21 +147,37 @@ float clamp(float x, float a, float b) {
     return min(max(x,a),b);
 }
 
+float length(vec3 v) {
+    return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+}
+
+float length(vec4 v) {
+    return sqrt(v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w);
+}
+
+vec3 normalize(vec3 v) {
+    return v / length(v);
+}
+
+vec4 normalize(vec4 v) {
+    return v / length(v);
+}
+
 int main(int argc, char *argv[]) {
     int width, height;
     std::cin >> width;
     std::cin >> height;
     
-    // render shader output to PPM
+    // render shader output to pbm (http://en.wikipedia.org/wiki/Netpbm_format)
     std::cout << "P3" << std::endl;
     std::cout << width << " " << height << std::endl;
     std::cout << 255 << std::endl;
     
-    vec3 C = vec3(0.0,-5.0,0.0);
+    C = vec3(0.0,-4.0,0.0);
     
-    for (float y = -1.0; y < 1.0; y += 2.0 / height) {
-        for (float x = -1.0; x < 1.0; x += 2.0 / width) {
-            D = vec3(x, 0.0, y) + C;
+    for (float y = -1.0; y < 1.0-0.0001; y += 2.0 / height) {
+        for (float x = -1.0; x < 1.0-0.0001; x += 2.0 / width) {
+            D = vec3(x, 0.0, y) - C;
             _main();
             
             float a = clamp(gl_FragColor.a, 0, 1);
--- frag.c
+++ frag.c
@@ -1,8 +1,8 @@
-#ifdef GPU
+#ifdef CPU
+#include "emulate.cpp_"
+#else
 uniform vec3 C;
 varying vec3 D;
-#else
-#include "emulate.cpp"
 #endif
 
 #define X(T) (C.x + T * D.x)
@@ -13,7 +13,7 @@ varying vec3 D;
     X(T) * X(T) + Y(T) * Y(T) + Z(T) * Z(T) - 1.0
 
 #define torus(C,D,T) \
-    pow(1 - sqrt(X*X + Y*Y), 2) + Z*Z - 0.1
+    pow(1 - sqrt(X(T)*X(T) + Y(T)*Y(T)), 2) + Z(T)*Z(T) - 0.1
 
 #define surface(C,D,T) \
     sphere(C,D,T)
@@ -22,59 +22,74 @@ void main() {
     // P(t) = C + t * D, t >= 0
     // find an interval for the secant method
     
-    //std::cerr << "--------------------" << std::endl;
+    #ifdef DEBUG
+    std::cerr << "--------------------" << std::endl;
+    #endif
+    
     // find nearest sign change with increasing multiples of two
     // probably should use some smaller multiple
     float t_low = 0.0, t_high = 1.0;
-    int sign = surface(C,D,t_low);
+    float sign = surface(C,D,t_low);
     for (; t_high <= 64.0; t_high *= 2.0) {
         int a = surface(C,D,t_high) > 0;
         int b = sign > 0;
+        
+        #ifdef DEBUG
+        std::cerr << "search t="<<t_low << "," << t_high << std::endl;
+        #endif
+        
         if ((a && !b) || (!a && b)) break;
         t_low = t_high;
     }
-    //std::cerr << t_low << "," << t_high << std::endl;
     
     // then binary search on that interval to a fixed resolution
-    while (t_high - t_low > 0.1) {
+    while (t_high - t_low > 0.01) {
         float mid = 0.5 * (t_high + t_low);
-        float mid_a = 0.5 * (t_high + t_low) + 0.01;
-        float mid_b = 0.5 * (t_high + t_low) - 0.01;
+        int a = surface(C,D,t_high)>0;
+        int b = surface(C,D,mid)>0;
+        // int c=surface(C,D,t_low)>0;
         
-        // follow lower side
-        if (surface(C,D,mid_a) > surface(C,D,mid_b)) {
+        if (a == b) { // transition is between mid and lo
             t_high = mid;
         }
-        else {
+        else { // transition is between hi and mid
             t_low = mid;
         }
+        
+        #ifdef DEBUG
+        std::cerr << "bisection t="<<t_low << "," << t_high << std::endl;
+        #endif
     }
-    //std::cerr << t_low << "," << t_high << std::endl;
     
-    float xn_a = surface(C,D,t_high);
-    float xn_b = surface(C,D,t_low);
+    float xn_a = t_high; // surface(C,D,t_high);
+    float xn_b = t_low; // surface(C,D,t_low);
     
     // use the secant method on this interval
     const float epsilon = 0.01;
     float d;
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 10; i++) {
         float f_a = surface(C,D,xn_a);
         float f_b = surface(C,D,xn_b);
         d = (xn_a - xn_b) / (f_a - f_b) * f_a;
-        if (abs(d) < epsilon) break; // found an acceptable root
+        if (abs(f_a-f_b) < epsilon) break; // found an acceptable root
         xn_b = xn_a;
         xn_a -= d;
+        
+        #ifdef DEBUG
+        std::cerr << "Secant t="<<xn_a << "," << xn_b << std::endl;
+        #endif
     }
-    //std::cerr << xn_a << "," << xn_b << std::endl;
     
     if (d != d) { // NaN
-        gl_FragColor = vec4(0.0,0.0,0.0,0.0);
+        gl_FragColor = vec4(1.0,1.0,0.0,0.0);
     }
     else if (abs(d) > epsilon) {
         gl_FragColor = vec4(0.0,1.0,0.0,1.0);
     }
     else {
-        vec3 point = C + xn_a * D; // point of intersection
-        gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+        vec3 point = C + t_high * D; // point of intersection
+        gl_FragColor = vec4(point.x,0.0,point.z,1.0);
     }
+    
+ //   gl_FragColor = vec4(xn_a,xn_b,0.0,1.0);
 }
