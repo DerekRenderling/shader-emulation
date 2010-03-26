a01f0f9839965335a5b13423e70be93eb69d2320
James Halliday <substack@gmail.com>
2010-03-26 14:40:30 -0800

    shader rewrite to support cpu emulation

--- sim/Shader.hs
+++ sim/Shader.hs
@@ -1,13 +1,18 @@
 module Shader (
-    Sources(..),
-    newProgram, newFromFiles, withProgram, bindProgram, bindvProgram
+    Sources(..), Prog(..),
+    newGPU, newGPU', newCPU,
+    withProgram, bindProgram, bindvProgram,
+    withTexture2D
 ) where
 import Graphics.UI.GLUT
 import Data.Maybe (isJust,fromJust)
-import Foreign (Ptr)
+import Foreign (Ptr,mallocBytes)
 import qualified Language.Preprocessor.Cpphs as C
 import Control.Applicative ((<$>))
 import Control.Monad.Error (ErrorT,throwError,liftIO)
+
+import System.Cmd.Utils (PipeHandle,hPipeBoth)
+import System.IO (Handle,hPrint,hPutStrLn,hGetBuf)
 
 data Sources = Sources {
     vFile :: FilePath,
@@ -17,34 +22,19 @@ data Sources = Sources {
     sourceDefs :: [(String,String)] -- pre-defined values
 }
 
+
+type CPUProgram = (PipeHandle, Handle, Handle)
+
+data Prog
+    = GPU { gpuProg :: Program }
+    | CPU { cpuProg :: CPUProgram }
+
 type ShaderT = ErrorT String IO
 
--- create a program from sources and options
-newProgram :: Sources -> ShaderT Program
-newProgram sources = do
-    vShader <- compile (vFile sources) sources
-    fShader <- compile (fFile sources) sources
-    
-    prog <- liftIO $ do
-        [prog'] <- genObjectNames 1
-        attachedShaders prog' $= ([vShader], [fShader])
-        linkProgram prog'
-        -- reportErrors
-        return prog'
-    
-    ((liftIO $ get $ linkStatus prog) >>=) $ \ok -> if ok
-        then return prog
-        else (throwError =<<)
-            $ ((++ "\nFailed to link program.\n") <$>)
-            $ liftIO (get $ programInfoLog prog)
-
--- create a program from vertex and fragment files and default options
-newFromFiles :: FilePath -> FilePath -> ShaderT Program
-newFromFiles = (newProgram .) . useFiles
-
-useFiles :: FilePath -> FilePath -> Sources
-useFiles v f =
-    Sources {
+-- | Create a gpu program from vertex and fragment files and default options
+newGPU :: FilePath -> FilePath -> ShaderT Prog
+newGPU v f = newGPU'
+    $ Sources {
         vFile = v,
         fFile = f,
         sourceOpts = C.defaultBoolOptions { C.locations = False },
@@ -52,26 +42,84 @@ useFiles v f =
         sourceDefs = []
     }
 
--- bind uniform variables to a program object
-bindProgram :: Uniform a => Program -> String -> a -> IO ()
-bindProgram prog key value = do
+-- | Create a gpu program from sources and options
+newGPU' :: Sources -> ShaderT Prog
+newGPU' sources = do
+    vShader <- compile (vFile sources) sources
+    fShader <- compile (fFile sources) sources
+    
+    prog <- liftIO $ do
+        [prog'] <- genObjectNames 1
+        attachedShaders prog' $= ([vShader], [fShader])
+        linkProgram prog'
+        return prog'
+    
+    ((liftIO $ get $ linkStatus prog) >>=) $ \ok -> if ok
+        then return $ GPU prog
+        else (throwError =<<)
+            $ ((++ "\nFailed to link program.\n") <$>)
+            $ liftIO (get $ programInfoLog prog)
+
+-- | Create an emulated shader from a command.
+-- The command should read (width,height) followed by the uniform parameters
+-- line-by-line as key=value. Output should be raw RGB bytes on stdout.
+newCPU :: FilePath -> [String] -> IO Prog
+newCPU cmd args = do
+    cpu@(_,_,fh) <- hPipeBoth cmd args
+    Size w h <- get windowSize
+    hPrint fh (w,h)
+    return $ CPU cpu
+
+-- | Bind uniform variables to a program object
+bindProgram :: (Uniform a, Show a) => Prog -> String -> a -> IO ()
+bindProgram (GPU prog) key value = do
     location <- get $ uniformLocation prog key
     reportErrors
     uniform location $= value
+bindProgram CPU{ cpuProg = (_,_,fh) } key value = do
+    hPutStrLn fh $ key ++ "=" ++ show value
 
--- bind uniform variables to a program object
-bindvProgram :: Uniform a => Program -> String -> Int -> Ptr a -> IO ()
-bindvProgram prog key size ptr = do
+-- | Bind uniform variables to a program object
+bindvProgram :: Uniform a => Prog -> String -> Int -> Ptr a -> IO ()
+bindvProgram (GPU prog) key size ptr = do
     location <- get $ uniformLocation prog key
     reportErrors
     uniformv location (fromIntegral size) ptr
+bindvProgram CPU{} key size ptr = fail "Not supported."
 
--- run a shader over some stateful operations
-withProgram :: Program -> IO () -> IO ()
-withProgram prog m = do
+-- | Run a shader over some stateful operations
+withProgram :: Prog -> IO () -> IO ()
+withProgram (GPU prog) f = do
     currentProgram $= Just prog
-    m
+    f
     currentProgram $= Nothing
+withProgram CPU{ cpuProg = (_,rh,wh) } f = do
+    size@(Size w h) <- get windowSize
+    hPrint wh (w,h)
+    ptr <- mallocBytes (fromIntegral $ 3 * w * h)
+    hGetBuf rh ptr (fromIntegral $ 3 * w * h)
+    withTexture2D size (PixelData RGB Byte ptr) f
+
+withTexture2D :: Size -> PixelData (Color3 GLubyte) -> IO () -> IO ()
+withTexture2D (Size w h) texData f = do
+    -- save texture capability
+    prevCap <- get $ texture Texture2D
+    texture Texture2D $= Enabled
+    
+    [tex] <- genObjectNames 1
+    textureBinding Texture2D $= Just tex
+    texImage2D
+        Nothing -- not a cube map
+        NoProxy -- standard texture 2d
+        0 -- level 0
+        RGB' -- internal format
+        (TextureSize2D w h) -- texture size
+        0 -- border
+        texData
+    f -- user geometry
+    
+    -- set texture capability back to whatever it was before
+    texture Texture2D $= prevCap
 
 -- compile a shader from source (hidden)
 compile :: Shader s => FilePath -> Sources -> ShaderT s
@@ -84,7 +132,6 @@ compile srcFile sources = do
                 [] [".","./glsl"] (sourceOpts sources)
             =<< readFile srcFile
         compileShader sh
-        -- reportErrors
         return sh
     
     ((liftIO $ get $ compileStatus shader) >>=) $ \ok -> if ok
