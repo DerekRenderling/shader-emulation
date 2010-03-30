a7b50629e5bc6022dbda9718f32c20d099228572
James Halliday <substack@gmail.com>
2010-03-29 17:45:35 -0800

    emulate rewritten for continuous compilation

--- glsl/emulate.hpp
+++ glsl/emulate.hpp
@@ -13,31 +13,8 @@ static vec3 C, D;
 
 void _main();
 
-int main(int argc, char *argv[]) {
-    // generate a ppm file sometimes
-    bool ppm = 0;
-    if (argc >= 2 && !strcmp(argv[1],"ppm")) ppm = 1;
-    
-    vec2 size;
-    std::cin >> size;
-    std::cin >> C;
-    vec3 dir;
-    std::cin >> dir;
-    
-    dir = normalize(dir);
-    
-    float theta = atan2(dir.x,dir.z);
-    float rho = atan2(dir.y,dir.z);
-    
-    if (ppm) {
-        // render shader output to ppm (http://en.wikipedia.org/wiki/Netpbm_format)
-        std::cout << "P3" << std::endl;
-        std::cout << size.x << " " << size.y << std::endl;
-        std::cout << 255 << std::endl;
-    }
-    
-    char *buf;
-    if (!ppm) buf = new char[int(3 * size.x * size.y)];
+char * render(vec2 size, vec3 C, vec3 dir) {
+    char *buf = new char[int(3 * size.x * size.y)];
     int offset = 0;
     
     // proportions independent of aspect ratios
@@ -53,27 +30,54 @@ int main(int argc, char *argv[]) {
             _main();
             
             float a = clamp(gl_FragColor.a, 0, 1);
-            unsigned int r =
+            buf[offset+0] = // red
                 clamp(gl_FragColor.r * 255, 0, 255) * a - 255 * (a - 1);
-            unsigned int g =
+            buf[offset+1] = // green
                 clamp(gl_FragColor.g * 255, 0, 255) * a - 255 * (a - 1);
-            unsigned int b =
+            buf[offset+2] = // blue
                 clamp(gl_FragColor.b * 255, 0, 255) * a - 255 * (a - 1);
-            if (ppm) {
-                std::cout << r << " " << g << " " << b << " ";
-            }
-            else {
-                buf[offset+0] = r;
-                buf[offset+1] = g;
-                buf[offset+2] = b;
-                offset += 3;
-            }
+            offset += 3;
         }
     }
     
-    if (!ppm) {
-        fwrite(buf, 3, size.x * size.y, stdout);
-        fflush(stdout);
+    return buf;
+}
+
+int main(int argc, char *argv[]) {
+    // generate a ppm file sometimes
+    bool ppm = 0;
+    if (argc >= 2 && !strcmp(argv[1],"ppm")) ppm = 1;
+    while (1) {
+        vec2 size;
+        std::cin >> size;
+        std::cin >> C;
+        vec3 dir;
+        std::cin >> dir;
+        dir = normalize(dir);
+        
+        char *im = render(size, C, dir);
+        
+        if (ppm) {
+            // render shader output to ppm (http://en.wikipedia.org/wiki/Netpbm_format)
+            std::cout << "P3" << std::endl;
+            std::cout << size.x << " " << size.y << std::endl;
+            std::cout << 255 << std::endl;
+            
+            for (int y = 0; y < size.y; y++) {
+                for (int x = 0; x < size.x; x++) {
+                    int offset = 3 * (y * size.x + x);
+                    unsigned int r = im[offset + 0];
+                    unsigned int g = im[offset + 1];
+                    unsigned int b = im[offset + 2];
+                    std::cout << r << " " << g << " " << b << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+        else {
+            fwrite(im, 3, size.x * size.y, stdout);
+            fflush(stdout);
+        }
     }
 }
 
